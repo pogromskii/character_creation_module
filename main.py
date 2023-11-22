842d0191b1e9be161b9c549f7d3ffa6c3a948ae6
Георгий Погромский <gpogromskiy@gmail.com>
2023-11-22 14:57:54 +0300

    Make annotation of the code. According to flake8

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from random import randint
 
 
-def attack(char_name, char_class):
+def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         return (
             f' {char_name} нанёс урон противнику равный'
@@ -18,7 +18,7 @@ def attack(char_name, char_class):
     return (f'{char_name} не смог атаковать')
 
 
-def defence(char_name, char_class):
+def defence(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         return f'{char_name} блокировал {10 + randint(5, 10)} ед. урона'
     if char_class == 'mage':
@@ -28,7 +28,7 @@ def defence(char_name, char_class):
     return f'{char_name} не смог блокировать урон'
 
 
-def special(char_name, char_class,):
+def special(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         return (
                f' {char_name} применил специальное умение'
@@ -47,7 +47,9 @@ def special(char_name, char_class,):
     return (f'{char_name} не применил специальное умение')
 
 
-def start_training(char_name, char_class, attack_point=5, protect=10):
+def start_training(
+        char_name: str, char_class: str, attack_point: int = 5,
+        protect: int = 10) -> str:
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -58,12 +60,12 @@ def start_training(char_name, char_class, attack_point=5, protect=10):
     print(
         'Введи одну из команд: attack — чтобы атаковать противника,'
         ' defence — чтобы блокировать атаку противника или'
-        ' special — чтобы использовать свою суперсилу.'
+        ' special — чтобы использовать свою суперсилу.',
     )
     print('Если не хочешь тренироваться, введи команду skip.')
-    cmd = None
+    cmd: str = None
     while cmd != 'skip':
-        cmd = input('Введи команду: ')
+        cmd: str = input('Введи команду: ')
         if cmd == 'attack':
             print(attack(char_name, char_class))
         if cmd == 'defence':
@@ -73,11 +75,11 @@ def start_training(char_name, char_class, attack_point=5, protect=10):
     return 'Тренировка окончена.'
 
 
-def choice_char_class():
-    approve_choice = None
-    char_class = None
+def choice_char_class() -> str:
+    approve_choice: str = ''
+    char_class: str = ''
     while approve_choice != 'y':
-        char_class = input(
+        char_class: str = input(
             'Введи название персонажа,'
             'за которого хочешь играть:'
             ' Воитель — warrior, Маг — mage,'
@@ -87,17 +89,17 @@ def choice_char_class():
             print(
                 'Воитель — дерзкий воин ближнего боя.'
                 ' Сильный, выносливый'
-                ' и отважный.'
+                ' и отважный.',
             )
         if char_class == 'mage':
             print(
                 'Маг — находчивый воин дальнего боя.'
-                ' Обладает высоким интеллектом.'
+                ' Обладает высоким интеллектом.',
             )
         if char_class == 'healer':
             print(
                 'Лекарь — могущественный заклинатель.'
-                ' Черпает силы из природы, веры и духов.'
+                ' Черпает силы из природы, веры и духов.',
             )
         approve_choice = input(
             'Нажми (Y), чтобы подтвердить выбор, или любую другую кнопку,'
@@ -109,12 +111,12 @@ def choice_char_class():
 def main():
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
-    char_name = input('...назови себя: ')
+    char_name: str = input('...назови себя: ')
     print(f'Здравствуй, {char_name}! '
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
-    char_class = choice_char_class()
+    char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
 
 
